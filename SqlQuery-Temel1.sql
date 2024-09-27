500c9c2bc76e2220a4c9c04dfea3443b2f280002
Kadir Fatih KÖSE <101864021+kadirfatihk@users.noreply.github.com>
2024-09-27 17:30:49 +0300

    Add files via upload

---
+++ SqlQuery-Temel1.sql
@@ -0,0 +1,15 @@
+﻿-- ÖDEV 1 --
+﻿SELECT * FROM Film
+WHERE NOT length  > 50 AND (rental_rate != 2.99 OR rental_rate != 4.99) ;
+
+-- ÖDEV 2 --
+﻿SELECT * FROM Film
+WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99 ,28.99) ;
+
+-- ÖDEV 3 --
+SELECT * FROM Film
+WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99 ;
+
+-- ÖDEV 4 --
+SELECT COUNT(city) FROM city
+WHERE city LIKE '%r' OR city LIKE '%R' ;
